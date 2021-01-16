6b97f0bcc6f43c383bfc688ef6b25531650fa59a
XiaoranMeng <xrmeng720@gmail.com>
2021-01-15 23:59:17 -0600

    Create order aggregate

---
+++ CoreShop/Core/Entities/OrderAggregate/Order.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Core.Entities.OrderAggregate
+{
+    public class Order : Entity
+    {
+        public Order(IReadOnlyList<OrderItem> orderItems, string buyerEmail, 
+            Address shoppingAddress, DeliveryMethod deliveryMethod, decimal subtotal)
+        {
+            OrderItems = orderItems;
+            BuyerEmail = buyerEmail;
+            ShoppingAddress = shoppingAddress;
+            DeliveryMethod = deliveryMethod;
+            Subtotal = subtotal;
+        }
+
+        /// <summary>
+        /// Used to retrive a list of orders for a user
+        /// No connection to identity that is in a seperate context boundary
+        /// </summary>
+        public string BuyerEmail { get; set; }
+
+        public DateTimeOffset OrderDate { get; set; } = DateTimeOffset.Now;
+
+        public Address ShoppingAddress { get; set; }
+
+        public DeliveryMethod DeliveryMethod { get; set; }
+
+        public decimal Subtotal { get; set; }
+
+        public OrderStatus Status { get; set; } = OrderStatus.Pending;
+
+        public string PaymentIntentId { get; set; }
+
+        public IReadOnlyList<OrderItem> OrderItems { get; set; }
+
+        public decimal GetTotal()
+        {
+            return Subtotal + DeliveryMethod.Price;
+        }
+    }
+}
---
+++ CoreShop/Core/Entities/OrderAggregate/OrderItem.cs
@@ -0,0 +1,22 @@
+﻿namespace Core.Entities.OrderAggregate
+{
+    public class OrderItem : Entity
+    {
+        public OrderItem()
+        {
+        }
+
+        public OrderItem(ProductItemOrdered itemOrdered, decimal price, int quantity)
+        {
+            this.itemOrdered = itemOrdered;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public ProductItemOrdered itemOrdered { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int Quantity { get; set; }
+    }
+}
